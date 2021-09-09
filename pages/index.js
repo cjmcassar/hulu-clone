851413e399cb019ecad01f6a72db33cd1dbce27e
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2021-09-09 17:22:24 +0100

    Added timestamp

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+// https://youtu.be/MqDlsjc8GLo?t=688
+
 export default function Home() {
   return (
     <div className={styles.container}>
